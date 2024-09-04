1f67773b50e49ae1cafdce38e0cb17848df80288
Dhalintin <uchexdhalitin@gmail.com>
2024-09-04 10:37:13 +0100

    Fix: fixed a bug

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // app.js
 const express = require('express');
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 const { ExpressPeerServer } = require('peer');
 
 // Initialize Express app
@@ -17,9 +17,9 @@ function setupPeerServer(server) {
 }
 
 // Routes
-app.get('/', (req, res) => {
-  res.redirect(`/${uuidv4()}`); // Generate and send a unique room ID
-});
+// app.get('/', (req, res) => {
+//   res.redirect(`/${uuidv4()}`); // Generate and send a unique room ID
+// });
 
 app.get('/:room', (req, res) => {
   res.render('index', { RoomId: req.params.room }); // Render the chat room
